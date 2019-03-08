ef75b293fec5a3bd8feab94ab3aba07556a0eb47
Alister Scott <alister.scott@dominos.com.au>
2019-03-08 16:16:45 +1000

    Initial code

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # cypress-demo
 A cypress.io demo project
+
+## How to run specs
+
+* `./node_modules/.bin/cypress open` to open the Cypress runner to run specs interactively
+* `./node_modules/.bin/cypress run` to run the specs 
---
+++ cypress/integration/test.js
@@ -0,0 +1,17 @@
+describe( 'WebDriverJsDemo', function() {
+	it( 'can wait for an element to appear', function() {
+        cy.visit('http://webdriverjsdemo.github.io'); 
+        cy.get('#elementappearschild').should('be.visible');
+	} );
+
+    it( 'can wait (10 seconds) for element to change text value', function() {
+        cy.visit('http://webdriverjsdemo.github.io');
+        cy.get('#elementchangestext', { timeout: 10000 }).should('be.visible').and('contain', 'new');
+    } );
+
+    // it( 'ignores alerts when leaving the page', function() {
+    //     cy.visit('http://webdriverjsdemo.github.io/leave'); 
+    //     cy.get('#homelink').click();
+    //     cy.contains('WebDriverJs Demo Page').should('be.visible');
+    // } );
+} );
